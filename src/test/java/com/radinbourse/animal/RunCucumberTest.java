4c115a55c9e4139a2b07ed5765d7c4006b9519f0
Hadi <h.safari@radinbourse.com>
2022-02-06 09:01:18 +0330

    migrate cucumber runner to Junit5

--- src/test/java/com/radinbourse/animal/RunCucumberTest.java
+++ src/test/java/com/radinbourse/animal/RunCucumberTest.java
@@ -1,17 +1,22 @@
 package com.radinbourse.animal;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
-@RunWith(Cucumber.class)
+import static io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
 @CucumberContextConfiguration
 @ContextConfiguration(classes = TestConfiguration.class)
-@CucumberOptions(tags = "not @Disabled", plugin = {"json:target/cucumber.json"})
+@SelectClasspathResource("com/radinbourse/animal")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @Disabled")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber.json")
 public class RunCucumberTest {
 
 }
